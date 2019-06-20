c07446099535ee68712c35e9f4408fc8b321e7b6
alamer <ivc_shherbakoviv@esrr.rzd>
2019-06-20 11:38:51 +0300

    feat(pattern): add builder pattern

---
+++ src/main/java/com/alamer/patterndemo/creational/builder/FtpConfig.java
@@ -0,0 +1,64 @@
+package com.alamer.patterndemo.creational.builder;
+
+public class FtpConfig {
+
+    private String username;
+    private String password;
+    private String url;
+    private int port;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public FtpConfig(FtpConfigBuilder builder) {
+        this.username = builder.username;
+        this.password = builder.password;
+        this.url = builder.url;
+        this.port = builder.port;
+    }
+
+    public static class FtpConfigBuilder {
+        private String username;
+        private String password;
+        private String url;
+        private int port;
+
+       public FtpConfigBuilder username(String username) {
+            this.username = username;
+            return this;
+        }
+
+        public FtpConfigBuilder password(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public FtpConfigBuilder url(String url) {
+            this.url = url;
+            return this;
+        }
+
+        public FtpConfigBuilder port(int port) {
+            this.port = port;
+            return this;
+        }
+
+        public FtpConfig build() {
+            FtpConfig config = new FtpConfig(this);
+            return config;
+        }
+    }
+}
---
+++ src/test/java/com/alamer/patterndemo/creational/builder/FtpConfigTest.java
@@ -0,0 +1,17 @@
+package com.alamer.patterndemo.creational.builder;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FtpConfigTest {
+
+
+    @Test
+    public void testBuilder(){
+        String username="username";
+        FtpConfig config=new FtpConfig.FtpConfigBuilder().username("username").build();
+        assertEquals(config.getUsername(),username);
+    }
+
+}
